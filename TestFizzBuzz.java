1fee6a9c74954f9f89ca12042d288166efbb039a
ddrgit <44521911+ddrgit@users.noreply.github.com>
2023-10-25 12:20:10 +0100

    Add files via upload

---
+++ TestFizzBuzz.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestFizzBuzz {
+    @Test
+    public void testPlay() {
+        String expected = "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz ";
+        Assertions.assertEquals(expected, play(15));
+    }
+
+    public static String play(int num1) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 1; i <= num1; i++) {
+            if (i % 15 == 0) {
+                result.append("FizzBuzz ");
+            } else if (i % 3 == 0) {
+                result.append("Fizz ");
+            } else if (i % 5 == 0) {
+                result.append("Buzz ");
+            } else {
+                result.append(i).append(" ");
+            }
+        }
+        return result.toString();
+    }
+}
